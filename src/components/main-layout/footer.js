9f9e6a7630141602f90716e2dbd19ac004d63d43
Pablo Fernandez <pablo.fernandezfernandez@outlook.com>
2023-10-04 19:24:08 +0200

    Adjust styles

--- src/components/main-layout/footer.js
+++ src/components/main-layout/footer.js
@@ -10,31 +10,37 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="layout-footer">
-        <section>
+        <section className="flex flex-col gap-1">
           <FooterHeader text="Horario" icon="clock" />
-          <p>Mañanas (Lunes - Jueves): 8:00 - 13:00</p>
-          <p>Tarder (Lunes - Jueves): 15:00 - 18:00</p>
-          <p>Viernes: 8:00 - 14:00</p>
+          <div>
+            <p>Mañanas (Lunes - Jueves): 8:00 - 13:00</p>
+            <p>Tarder (Lunes - Jueves): 15:00 - 18:00</p>
+            <p>Viernes: 8:00 - 14:00</p>
+          </div>
         </section>
-        <section>
+        <section className="flex flex-col gap-1">
           <FooterHeader text="Ubicación" icon="location-dot" />
-          <p>
-            Parque empresarial de Morero P.2-11, Nave 2
-            <a
-              className="ml-2"
-              target="_blank"
-              href="https://www.google.com/maps/place/Castrol+Service+motec+s.l.+Motor+y+Tecnologia+del+Automovil/@43.396785,-3.8393119,17z/data=!3m1!4b1!4m6!3m5!1s0xd4936420c1708fb:0xbc37d36d1444f5f6!8m2!3d43.396785!4d-3.836737!16s%2Fg%2F11btm3j378?entry=ttu"
-            >
-              <FaIcon icon="up-right-from-square"></FaIcon>
-            </a>
-          </p>
-          <p>39611 Guarnizo</p>
-          <p>Cantrabria</p>
+          <div>
+            <p>
+              Parque empresarial de Morero P.2-11, Nave 2
+              <a
+                className="ml-2"
+                target="_blank"
+                href="https://www.google.com/maps/place/Castrol+Service+motec+s.l.+Motor+y+Tecnologia+del+Automovil/@43.396785,-3.8393119,17z/data=!3m1!4b1!4m6!3m5!1s0xd4936420c1708fb:0xbc37d36d1444f5f6!8m2!3d43.396785!4d-3.836737!16s%2Fg%2F11btm3j378?entry=ttu"
+              >
+                <FaIcon icon="up-right-from-square"></FaIcon>
+              </a>
+            </p>
+            <p>39611 Guarnizo</p>
+            <p>Cantrabria</p>
+          </div>
         </section>
-        <section>
+        <section className="flex flex-col gap-1">
           <FooterHeader text="Contacto" icon="phone" />
-          <p>942566299</p>
-          <p>info@motec.es</p>
+          <div>
+            <p>942566299</p>
+            <p>info@motec.es</p>
+          </div>
         </section>
       </footer>
     );
