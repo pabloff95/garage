f32e8c758b69ecde5b9471c0916ead93eedd8eeb
Pablo Fernandez <pfernandez@dataahead.de>
2024-01-16 20:35:19 +0100

    Style and add basic content to not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FaIcon from "../components/basic-elements/fa-icon";
 
 class NotFound extends React.Component {
   constructor(props) {
@@ -7,8 +8,16 @@ class NotFound extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Page not found</h1>
+      <div className="h-[90vh]">
+        <div className="h-full flex flex-col justify-center items-center gap-4">
+          <div className="h-[30vh] w-[30vh] bg-primary-element rounded-full z-0 flex justify-center items-center">
+            <FaIcon
+              icon="square-xmark"
+              className="h-[23vh] z-10 !opacity-100"
+            />
+          </div>
+          <h1 className="font-bold text-3xl">Página no encontrada</h1>
+        </div>
       </div>
     );
   }
