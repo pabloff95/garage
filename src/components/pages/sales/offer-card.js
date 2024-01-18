339c5515619b6bf0c640885cbb4eb34f8d0e5055
Pablo Fernandez <pfernandez@dataahead.de>
2024-01-18 19:19:09 +0100

    Add reduced price as percentage to offer card

--- src/components/pages/sales/offer-card.js
+++ src/components/pages/sales/offer-card.js
@@ -5,6 +5,15 @@ class OfferCard extends React.Component {
     super(props);
   }
 
+  getReducedPrice = () => {
+    // Show reduced price as percentage
+    const { originalPrice, currentPrice } = this.props;
+
+    const reducedPrice = 100 - (currentPrice * 100) / originalPrice;
+
+    return reducedPrice ? `-${reducedPrice}%` : "";
+  };
+
   render() {
     const { title, description, originalPrice, currentPrice, imgPath } =
       this.props;
@@ -13,7 +22,12 @@ class OfferCard extends React.Component {
       <div>
         <div className="w-fit shadow-neutral-color-gray">
           <main className="flex flex-col gap-2 items-center border rounded border-primary-element p-4">
-            <section className="h-[40vh]">
+            <section className="h-[40vh] w-fit relative">
+              {this.getReducedPrice() && (
+                <div className="top-4 right-0 absolute bg-red-500 text-white font-bold text-xl px-2 py-1 rounded-l-md">
+                  {this.getReducedPrice()}
+                </div>
+              )}
               <img src={imgPath} className="h-full" />
             </section>
             <section>
