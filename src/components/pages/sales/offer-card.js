ebbca00cf1e28c1a032f88fbe4f70e7b9c1148a6
Pablo Fernandez <pfernandez@dataahead.de>
2024-01-25 21:42:20 +0100

    Install moment js. Display only valid offers according to the date range. Add countdown text to offer card.

--- src/components/pages/sales/offer-card.js
+++ src/components/pages/sales/offer-card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FaIcon from "../../basic-elements/fa-icon";
 
 class OfferCard extends React.Component {
   constructor(props) {
@@ -9,17 +10,33 @@ class OfferCard extends React.Component {
     // Show reduced price as percentage
     const { originalPrice, currentPrice } = this.props;
 
-    const reducedPrice = 100 - (currentPrice * 100) / originalPrice;
+    const reducedPrice = Math.round(100 - (currentPrice * 100) / originalPrice);
 
     return reducedPrice ? `-${reducedPrice}%` : "";
   };
 
   render() {
-    const { title, description, originalPrice, currentPrice, imgPath } =
-      this.props;
+    const {
+      title,
+      description,
+      originalPrice,
+      currentPrice,
+      imgPath,
+      daysLeft,
+    } = this.props;
 
     return (
-      <div>
+      <div className="min-w-[30%]">
+        <div className="font-bold text-xl py-1">
+          <div className="relative h-fit flex flex-row w-full justify-center items-end mb-2">
+            <span>¡Solo quedan </span>
+            <span className="relative flex justify-center w-[3rem]">
+              <FaIcon icon="calendar" className="absolute h-full" />
+              <span className="text-white !z-50 mt-[1rem]">{daysLeft}</span>
+            </span>
+            <span> días!</span>
+          </div>
+        </div>
         <div className="w-fit shadow-neutral-color-gray">
           <main className="flex flex-col gap-2 items-center border rounded border-primary-element p-4">
             <section className="h-[40vh] w-fit relative">
--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -1,8 +1,33 @@
 import React from "react";
 import OfferCard from "../components/pages/sales/offer-card";
 import * as offers from "../data/offers.json";
+import moment from "moment";
 
 class Sales extends React.Component {
+  getDaysLeft = (endDate) => {
+    const endDateMoment = moment(`${endDate}/${moment().year()}`, "DD/MM/YYYY");
+
+    const currentDateMoment = moment();
+
+    const daysLeft = endDateMoment.diff(currentDateMoment, "days");
+
+    return daysLeft;
+  };
+
+  getValidOffers = () => {
+    // Get valid offers (date is in the valid range when compared to the current date)
+    const validOffers = offers.default.filter((offer) => {
+      const daysLeft = this.getDaysLeft(offer.endDate);
+
+      return daysLeft > 0;
+    });
+
+    return validOffers.map((offer) => ({
+      ...offer,
+      daysLeft: this.getDaysLeft(offer.endDate),
+    }));
+  };
+
   render() {
     return (
       <div className="mt-[5vh]">
@@ -10,14 +35,15 @@ class Sales extends React.Component {
           <h1 className="w-80% mx-[10%] font-bold text-3xl my-3">Ofertas</h1>
         </section>
         <section className="my-5 w-full py-3">
-          <div className="w-80% mx-[10%] flex flex-row justify-center gap-6 items-center">
-            {offers.default.map((offer, index) => {
+          <div className="w-80% mx-[10%] flex flex-row flex-wrap justify-center gap-6 items-center">
+            {this.getValidOffers().map((offer, index) => {
               const {
                 title,
                 description,
                 originalPrice,
                 currentPrice,
                 imgPath,
+                daysLeft,
               } = offer;
 
               return (
@@ -27,6 +53,7 @@ class Sales extends React.Component {
                   originalPrice={originalPrice}
                   currentPrice={currentPrice}
                   imgPath={imgPath}
+                  daysLeft={daysLeft}
                   key={`offer-card-${index}`}
                 />
               );
