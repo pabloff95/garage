0292dce3bd7ba71c2e2e461de99e874b1ccf49e9
Pablo Fernandez <pfernandez@dataahead.de>
2024-02-01 18:48:54 +0100

    Replace import of all icons from Font Awesome by only those used in the app

--- src/components/basic-elements/fa-icon.js
+++ src/components/basic-elements/fa-icon.js
@@ -1,13 +1,63 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import * as Icons from "@fortawesome/free-solid-svg-icons";
+import {
+  faCopy,
+  faTimes,
+  faClock,
+  faLocationDot,
+  faUpRightFromSquare,
+  faPhone,
+  faStar,
+  faMagnifyingGlassPlus,
+  faRankingStar,
+  faHandshake,
+  faPeopleGroup,
+  faIdBadge,
+  faEnvelope,
+  faCar,
+  faOilCan,
+  faScrewdriverWrench,
+  faArrowTrendUp,
+  faPlus,
+  faCircleDot,
+  faSquareXmark,
+} from "@fortawesome/free-solid-svg-icons";
+// import * as Icons from "@fortawesome/free-solid-svg-icons"; See comment bellow
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
-// Import all icons from https://fontawesome.com
-const iconList = Object.keys(Icons)
-  .filter((key) => key !== "fas" && key !== "prefix")
-  .map((icon) => Icons[icon]);
+// Import only the used icons to avoid loading all the icons when loading the pages. New icons should be imported
+// from "@fortawesome/free-solid-svg-icons" and then added to "iconList". In case of wanting to import them all uncomment the following code.
+
+/* 
+  Import all icons from https://fontawesome.com
+  const iconList = Object.keys(Icons)
+    .filter((key) => key !== "fas" && key !== "prefix")
+    .map((icon) => Icons[icon]);
+*/
+
+const iconList = [
+  faCopy,
+  faTimes,
+  faClock,
+  faLocationDot,
+  faUpRightFromSquare,
+  faPhone,
+  faStar,
+  faMagnifyingGlassPlus,
+  faRankingStar,
+  faHandshake,
+  faPeopleGroup,
+  faIdBadge,
+  faEnvelope,
+  faCar,
+  faOilCan,
+  faScrewdriverWrench,
+  faArrowTrendUp,
+  faPlus,
+  faCircleDot,
+  faSquareXmark,
+];
 
 // Brand icons belong to a different module. Add only those used in the app
 iconList.push(faFacebook);
