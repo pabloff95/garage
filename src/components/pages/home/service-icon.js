ba90425e50f06729f7c27a2d195b3be4acc2e9b2
Pablo Fernandez <pfernandez@dataahead.de>
2024-04-29 21:35:53 +0200

    Add responsive design to home page

--- src/components/pages/home/service-icon.js
+++ src/components/pages/home/service-icon.js
@@ -8,9 +8,9 @@ class ServiceIcon extends React.Component {
 
   render() {
     return (
-      <div className="flex flex-col gap-3 items-center text-zinc-800">
-        <FaIcon icon={this.props.icon} className="h-11" />
-        <span className="text-lg font-sans font-semibold">
+      <div className="basis-[100%] min-[300px]:basis-[50%] my-2 sm:my-0 sm:basis-[unset] flex flex-col sm:gap-3 items-center text-zinc-800">
+        <FaIcon icon={this.props.icon} className="h-6 sm:h-8 md:h-11" />
+        <span className="text-sm sm:text-md md:text-lg font-sans font-semibold">
           {this.props.iconText}
         </span>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -55,15 +55,15 @@ class Home extends React.Component {
   getItemsPerSlide = () => {
     const windowWidth = parseInt(window.innerWidth);
 
-    if (windowWidth < 500) {
+    if (windowWidth < 823) {
       return 1;
     }
 
-    if (windowWidth < 850) {
+    if (windowWidth < 1253) {
       return 2;
     }
 
-    if (windowWidth < 1600) {
+    if (windowWidth < 1650) {
       return 3;
     }
 
@@ -113,104 +113,109 @@ class Home extends React.Component {
       <div>
         <section className="w-full">
           <div className="w-full h-fit relative">
-            <div className="h-fit w-fit z-10 absolute left-[15%] top-[20%] flex flex-col gap-2">
-              <h1 className="text-neutral-color-contrast text-4xl tracking-wide text-shadow-neutral-color-primary">
+            <div className="p-6 md:p-0 h-full md:h-fit w-full md:w-fit z-10 absolute md:left-[5%] lg:left-[15%] top-[20%] flex flex-col md:gap-2 align-center">
+              <h1 className="text-neutral-color-contrast text-3xl sm:text-4xl tracking-wide text-shadow-neutral-color-primary">
                 Taller mecánico
               </h1>
-              <h2 className="font-rubik-mono-one text-primary-element text-8xl tracking-wide text-shadow-neutral-color-primary-title">
+              <h2 className="font-rubik-mono-one text-primary-element text-5xl sm:text-6xl lg:text-8xl tracking-wide text-shadow-neutral-color-primary-title">
                 MOTEC
               </h2>
-              <h3 className="text-neutral-color-contrast text-3xl text-shadow-neutral-color-primary">
+              <h3 className="text-neutral-color-contrast text-xl sm:text-3xl text-shadow-neutral-color-primary">
                 Servicios de mantenimiento y reparación para tu vehículo
               </h3>
               <div className="h-1 w-full bg-[#f2f2f2] mt-2 mb-14 rounded"></div>
-              <Link
-                className="text-neutral-color-contrast border-neutral-color-contrast hover:border-element-gained-focus hover:text-element-gained-focus hover:scale-95 hover:text-shadow-secondary-element transition-all duration-200 w-fit p-3 rounded font-bold tracking-wider"
-                to="/contact"
-                data-tooltip-content="Abrir página de contacto"
-                data-tooltip-id="to-contact-link-tooltip"
-                data-tooltip-place="bottom"
-              >
-                CONTACTANOS
-                <Tooltip id="to-contact-link-tooltip" />
-              </Link>
+              <div className="text-center sm:text-left">
+                <Link
+                  className="text-neutral-color-contrast border-neutral-color-contrast hover:border-element-gained-focus hover:text-element-gained-focus hover:scale-95 hover:text-shadow-secondary-element transition-all duration-200 w-fit p-3 rounded font-bold tracking-wider"
+                  to="/contact"
+                  data-tooltip-content="Abrir página de contacto"
+                  data-tooltip-id="to-contact-link-tooltip"
+                  data-tooltip-place="bottom"
+                >
+                  CONTACTANOS
+                  <Tooltip id="to-contact-link-tooltip" />
+                </Link>
+              </div>
             </div>
             <img
-              className="w-full grayscale opacity-90"
+              className="w-full min-h-[60vh] lg:min-h-fit grayscale opacity-90"
               src="/images/pages/home/home-header.jpg"
               alt="Instalaciones de talleres Motec"
             />
           </div>
         </section>
-        <section className="mb-10">
-          <div className="p-3 w-full px-[15%] py-5 bg-[#e2e2e2a4] flex flex-col gap-10">
-            <h1 className="w-full font-bold text-3xl text-center my-3">
-              Nuestros servicios
-            </h1>
-            <div className="w-full h-full flex flex-row justify-between">
+        <section className="pb-3 sm:pb-5 w-full">
+          <div className="p-3 w-full px-[5%] lg:px-[15%] py-5 bg-[#e2e2e2a4] flex flex-col">
+            <div className="w-full h-full flex flex-row flex-wrap justify-between">
               <ServiceIcon icon="car" iconText="Mecánica" />
-              <ServiceIcon icon="oil-can" iconText="Cambio de aceite" />
-              <ServiceIcon
-                icon="circle-dot"
-                iconText="Servicio de neumáticos"
-              />
+              <ServiceIcon icon="oil-can" iconText="Aceite" />
+              <ServiceIcon icon="circle-dot" iconText="Neumáticos" />
               <ServiceIcon icon="screwdriver-wrench" iconText="Mantenimiento" />
               <ServiceIcon icon="arrow-trend-up" iconText="Puesta a punto" />
               <ServiceIcon icon="plus" iconText="Y mas!" />
             </div>
           </div>
           <div className="p-3 w-full h-4 bg-gradient-to-b from-[#e2e2e2a4] to-[#fbf8f8]"></div>
-          <p className="w-80% mx-[10%] text-lg px-3">
-            En nuestro taller ofrecemos una amplia gama de servicios, desde
-            cambios de aceite y frenos hasta reparaciones de motor y
-            transmisión, para asegurarnos de que tu vehículo funcione de manera
-            segura y eficiente en todo momento.
-            <br />
-            <br />
-            Utilizamos tecnología de vanguardia y piezas de alta calidad para
-            garantizar resultados duraderos. Nuestros mecánicos se enorgullecen
-            de mantener tus vehículos en óptimas condiciones y brindar
-            soluciones confiables para todas tus necesidades.
-          </p>
         </section>
-        <section className="my-5 p-3">
-          <h1 className="w-full font-bold text-3xl text-center my-3">
-            Encuentranos en Astillero (Santander)
-          </h1>
-          <p className="w-80% mx-[10%] text-lg p-3">
-            ¡Ven a cononcernos! Nos ubicamos en el parque empresarial de Morero,
-            parcela 2-11 nave nº2, Guarnizo el Astillero:
-          </p>
-          <div className="w-full text-center">
-            <GarageMap
-              mapClasses="w-1/2 h-96 m-auto"
-              zoom="13"
-              showLink={true}
-            />
+        <section className="pb-3 sm:pb-5 w-full">
+          <div className="w-80% mx-[10%] flex flex-col gap-2">
+            <h2 className="text-2xl font-semibold">Nuestros servicios</h2>
+            <p>
+              En nuestro taller ofrecemos una amplia gama de servicios, desde
+              cambios de aceite y frenos hasta reparaciones de motor y
+              transmisión, para asegurarnos de que tu vehículo funcione de
+              manera segura y eficiente en todo momento.
+              <br />
+              <br />
+              Utilizamos tecnología de vanguardia y piezas de alta calidad para
+              garantizar resultados duraderos. Nuestros mecánicos se
+              enorgullecen de mantener tus vehículos en óptimas condiciones y
+              brindar soluciones confiables para todas tus necesidades.
+            </p>
           </div>
         </section>
-        <section className="flex flex-col p-3 justify-center">
-          <h1 className="w-full font-bold text-3xl text-center  my-3">
-            La opinion de nuestros clientes
-          </h1>
-          <p className="w-80% mx-[10%] text-lg p-3">
-            Tu satisfacción es nuestra prioridad. En talleres Motec nos
-            enorgullecemos de ofrecer servicios de alta calidad a precios
-            competitivos. Descubre por qué somos la elección preferida de
-            nuestros clientes cuando se trata de cuidar sus vehículos:
-          </p>
-          <HorizontalScrollContainer
-            key={this.state.itemsPerSlide}
-            itemsPerSlide={this.state.itemsPerSlide}
-            className="w-full py-5 my-5 horizontal-scroll-container"
-            onSlideChange={() => this.updateReviewCardHeight(0)}
-            tooltipMessage="Abrir review en Google"
-            hidePagination={true}
-            autoPlay={false}
-            renderAsGrid={true}
-          >
-            {this.getReviewCards()}
-          </HorizontalScrollContainer>
+        <section className="pb-3 sm:pb-5 w-full">
+          <div className="w-80% mx-[10%] flex flex-col gap-2">
+            <h2 className="text-2xl font-semibold">
+              Encuentranos en Astillero (Santander)
+            </h2>
+            <p>
+              ¡Ven a cononcernos! Nos ubicamos en el parque empresarial de
+              Morero, parcela 2-11 nave nº2, Guarnizo el Astillero:
+            </p>
+            <div className="w-full">
+              <GarageMap
+                mapClasses="w-full h-96 m-auto"
+                zoom="13"
+                showLink={true}
+              />
+            </div>
+          </div>
+        </section>
+        <section className="pb-3 sm:pb-5 w-full">
+          <div className="w-80% mx-[10%] flex flex-col gap-2">
+            <h2 className="text-2xl font-semibold">
+              La opinion de nuestros clientes
+            </h2>
+            <p>
+              Tu satisfacción es nuestra prioridad. En talleres Motec nos
+              enorgullecemos de ofrecer servicios de alta calidad a precios
+              competitivos. Descubre por qué somos la elección preferida de
+              nuestros clientes cuando se trata de cuidar sus vehículos:
+            </p>
+            <HorizontalScrollContainer
+              key={this.state.itemsPerSlide}
+              itemsPerSlide={this.state.itemsPerSlide}
+              className="w-full py-5 my-5 horizontal-scroll-container"
+              onSlideChange={() => this.updateReviewCardHeight(0)}
+              tooltipMessage="Abrir review en Google"
+              hidePagination={false}
+              autoPlay={false}
+              renderAsGrid={true}
+            >
+              {this.getReviewCards()}
+            </HorizontalScrollContainer>
+          </div>
         </section>
       </div>
     );
